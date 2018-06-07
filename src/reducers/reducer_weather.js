a5739651a063d2249bd5c5411ffda8a8cfb9a78d
StephenDK <stephendklein08@gmail.com>
2018-06-06 20:04:27 -0700

    Avoiding state mutations

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,9 +1,17 @@
+import { FETCH_WEATHER } from '../actions/index';
+
 // reducers are just functions
 // first argument is state which we default to null
 // second argument is action
-export default function(state = null, action) {
+export default function(state = [], action) {
     // Uncomment to see the payload from the action creator
-    console.log('Action received', action);
+    // console.log('Action received', action);
+    switch (action.type) {
+    case FETCH_WEATHER:
+        // return state.concat([action.payload.data]);
+        // Or ES6 version
+        return [ action.payload.data, ...state ];
+    }
     return state;
 }
 // add this reducer to the combine reducers call in index.js
